88396959507308d1a037fc97061596745b0eaa54
mohamed magdy <prof.mohamedmagdy@gmail.com>
2019-12-06 19:41:39 +0300

    discoverable device and paird devices

--- MyApplication2/MyApplication/app/src/main/java/com/example/myapplication/Activity2.java
+++ MyApplication2/MyApplication/app/src/main/java/com/example/myapplication/Activity2.java
@@ -9,13 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.bluetooth.BluetoothDevice;
+import java.util.Set;
 public class Activity2 extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE_BT=0;
+    private static final int REQUEST_ENABLE_BT=0 ,REQUEST_DISCOVER_BT=0;
     //private static final int REQUEST_DISABLE_BT=0;
-
+    TextView mStatusBlueTv, mPairedTv;
     Button button;
+    Button mPairedBtn,mDiscoverBtn;
     BluetoothAdapter bluetoothAdapter;
 
     TextView btstatus;
@@ -26,6 +28,7 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
 
         btstatus = findViewById(R.id.status);
+        mPairedTv     = findViewById(R.id.pairedTv);
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null){
@@ -33,7 +36,7 @@ public class Activity2 extends AppCompatActivity {
         }else{
             btstatus.setText("Bluetooth is available");
         }
-
+        mPairedBtn = (Button) findViewById(R.id.mPairedBtnid);
         button = (Button) findViewById(R.id.onoff);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +53,38 @@ public class Activity2 extends AppCompatActivity {
                 }
             }
         });
+        mDiscoverBtn = findViewById(R.id.available);
+        mDiscoverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!bluetoothAdapter.isDiscovering()){
+                    showToast("Making Your Device Discoverable");
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(intent, REQUEST_DISCOVER_BT);
+                }
+            }
+        });
+        //get paired devices btn click
+        mPairedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bluetoothAdapter.isEnabled()){
+                    mPairedTv.setText("Paired Devices");
+                    Set<BluetoothDevice> devices = bluetoothAdapter.getBondedDevices();
+                    for (BluetoothDevice device: devices){
+                        mPairedTv.append("\nDevice: " + device.getName()+ ", " + device);
+                    }
+                }
+                else {
+                    //bluetooth is off so can't get paired devices
+                    showToast("Turn on bluetooth to get paired devices");
+                }
+            }
+        });
+
     }
+
+
 
     /*public void enableDisableBT(){
         if(!bluetoothAdapter.isEnabled()){
@@ -62,4 +96,8 @@ public class Activity2 extends AppCompatActivity {
     private void showToast(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
+
+
+
+
 }
